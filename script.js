6a0fab49f73002c93ee2c486508141eb50ac44dd
muratalisise <92972964+muratalisise@users.noreply.github.com>
2024-04-29 16:57:11 +0300

    weather-project-finished

--- script.js
+++ script.js
@@ -1,53 +1,100 @@
-document.addEventListener("DOMContentLoaded", () => {
-	axios.get(
-		"https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=gerze",
-		{
-			headers: {
-				authorization: "apikey 1CejPfw1hYhgpjNLCrFJUz:2V0aFWLUzLEd7yOnver5CN",
-				"content-type": "application/json",
-			},
-		}
-	)
-		.then((response) => {
-			console.log(response.data);
-			console.log(response.data.result);
-			weatherConditions(response.data.result);
-			updateHeader(response.data.result[0], response.data.city);
+const cityDom = document.getElementById('city');
+const districtDom = document.getElementById('district');
 
-			response.data.result.forEach(day => {
-				const status = day.status;
-				//console.log(status);
-			});
+document.addEventListener("DOMContentLoaded", async () => {
+	const weather = (await axios.get("https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=gerze", {
+		headers: {
+			authorization: "apikey 1CejPfw1hYhgpjNLCrFJUz:2V0aFWLUzLEd7yOnver5CN",
+			"content-type": "application/json",
+		},
+	})).data.result;
 
-		})
-		.catch((error) => {
-			console.log(error.response);
-		});
-})
+	weatherConditions(weather)
 
-function updateHeader(dailyWeather, city) {
-	const cityelement = document.querySelector(".city");
-	const degree = document.querySelector(".degree");
-	const weatherDescription = document.querySelector(".weather-description");
-	const humidity = document.querySelector(".humidity");
-	const image = document.getElementById("image");
-	const iconUrl = getWeatherIcon(dailyWeather.status);
+	const cities = (await axios.get("https://api.kadircolak.com/Konum/JSON/API/ShowAllCity")).data;
 
-	cityelement.textContent = city;
-	degree.textContent = dailyWeather.degree + "°";
-	weatherDescription.textContent = dailyWeather.description;
-	humidity.innerHTML = `<i class="fa-solid fa-droplet icon"></i> ${dailyWeather.humidity}`;
-	image.src = iconUrl;
+	let citiesOptionsString = '<option value="" disabled>İl Seçiniz</option>';
+	for (let i = 0; i < cities.length; i++) {
+		citiesOptionsString += `<option value="${cities[i].ID}">${cities[i].TEXT}</option>`;
+	}
 
+	cityDom.innerHTML = citiesOptionsString;
+	cityDom.value = 57;
+
+	let districtsResponse = await axios.get(`https://api.kadircolak.com/Konum/JSON/API/ShowDistrict?plate=${cityDom.value}`);
+	const districts = districtsResponse.data;
+
+	let districtsOptionsString = '<option value="" disabled>İlçe Seçiniz</option>';
+	for (let i = 0; i < districts.length; i++) {
+		districtsOptionsString += `<option value="${districts[i].DISTRICT}">${districts[i].DISTRICT}</option>`;
+	}
+	districtDom.innerHTML = districtsOptionsString;
+	districtDom.value = "GERZE";
+});
+
+districtDom.onchange = async function () {
+	const weatherResponse = await axios.get(`https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${districtDom.value}`, {
+		headers: {
+			authorization: "apikey 1CejPfw1hYhgpjNLCrFJUz:2V0aFWLUzLEd7yOnver5CN",
+			"content-type": "application/json",
+		},
+	});
+	const weather = weatherResponse.data.result;
+
+	if (weather === undefined) {
+		alert("Seçilen bölgenin hava durumu verisi bulunamadı...");
+		return;
+	}
+	weatherConditions(weather)
+}
+
+cityDom.onchange = async () => {
+	districtDom.innerHTML = '<option value="">İlçe Seçiniz</option>';
+
+	let districtsResponse = await axios.get(`https://api.kadircolak.com/Konum/JSON/API/ShowDistrict?plate=${cityDom.value}`);
+	const districts = districtsResponse.data;
+
+	let districtsOptionsString = '<option value="" disabled>İlçe Seçiniz</option>';
+	for (let i = 0; i < districts.length; i++) {
+		districtsOptionsString += `<option value="${districts[i].DISTRICT}">${districts[i].DISTRICT}</option>`;
+	}
+	districtDom.innerHTML = districtsOptionsString;
+	districtDom.onchange();
 }
 
 function weatherConditions(weatherData) {
 	const weatherBody = document.querySelector(".weather-body");
 	let weatherHtml = "";
 
-	weatherData.forEach((element) => {
-		const iconUrl = getWeatherIcon(element.status);
-		console.log(`${element.status} , ${iconUrl}`);
+	weatherData.slice(0, 7).forEach((element) => {
+		let iconUrl;
+		switch (element.status) {
+			case "Clouds":
+				iconUrl = `./gif/icons8-cloud-64.png`;
+				break;
+			case "Cloudshot":
+				iconUrl = `./gif/icons8-cloudshot-64.png`;
+				break;
+			case "Night":
+				iconUrl = `./gif/icons8-night-64.png`;
+				break;
+			case "Clear":
+				iconUrl = `./gif/icons8-partly-cloudy-day-64.png`;
+				break;
+			case "Rain":
+				iconUrl = `./gif/icons8-rain-64.png`;
+				break;
+			case "Snow":
+				iconUrl = `./gif/icons8-snow-64.png`;
+				break;
+			case "Storm":
+				iconUrl = `./gif/icons8-storm-64.png`;
+				break;
+			default:
+				iconUrl = `./gif/icons8-cloud-64.png`;
+				break;
+		}
+		//console.log(`${element.status} , ${iconUrl}`);
 		weatherHtml += `
 		<div class="weather">
 			<div class="weather-info">
@@ -65,38 +112,4 @@ function weatherConditions(weatherData) {
 		`;
 	});
 	weatherBody.innerHTML = weatherHtml;
-}
-
-function getWeatherIcon(weatherStatus) {
-	//console.log("calstı");
-	let iconUrl;
-
-	switch (weatherStatus) {
-		case "Clouds":
-			iconUrl = `./gif/icons8-cloud-64.png`;
-			break;
-		case "Cloudshot":
-			iconUrl = `./gif/icons8-cloudshot-64.png`;
-			break;
-		case "Night":
-			iconUrl = `./gif/icons8-night-64.png`;
-			break;
-		case "Clear":
-			iconUrl = `./gif/icons8-partly-cloudy-day-64.png`;
-			break;
-		case "Rain":
-			iconUrl = `./gif/icons8-rain-64.png`;
-			break;
-		case "Snow":
-			iconUrl = `./gif/icons8-snow-64.png`;
-			break;
-		case "Storm":
-			iconUrl = `./gif/icons8-storm-64.png`;
-			break;
-		default:
-			iconUrl = `./gif/icons8-cloud-64.png`;
-			break;
-	}
-	//console.log(`${weatherStatus} , ${iconUrl}`);
-	return iconUrl;
 }
